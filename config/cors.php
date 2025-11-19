0c530ea7dfd2a447d888a2b5464f0eda86634976
Jamesnl55555 <jamesnl55555@gmail.com>
2025-11-19 18:46:29 +0800

    modified cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://frontend3rdyear-jamesnl55555s-projects.vercel.app'],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
