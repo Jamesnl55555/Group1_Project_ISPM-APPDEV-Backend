08feeeaad0b10e90f028ea85fa80baa57f1ccfc7
Jamesnl55555 <jamesnl55555@gmail.com>
2025-11-20 04:34:40 +0800

    modified cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://ispmappdevfrontend.vercel.app'],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
