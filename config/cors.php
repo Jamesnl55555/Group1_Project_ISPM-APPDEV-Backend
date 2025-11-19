37fe2dbea35f272e8a433510d6832de5bb668970
Jamesnl55555 <jamesnl55555@gmail.com>
2025-11-20 07:33:53 +0800

    modified cors

--- config/cors.php
+++ config/cors.php
@@ -6,6 +6,7 @@ return [
     'allowed_origins' => [],
     'allowed_origins_patterns' => ['/https:\/\/.*\.vercel\.app$/'],
     'allowed_headers' => ['*'],
+    'with_credentials' => true,
     'supports_credentials' => true,
 ];
 
