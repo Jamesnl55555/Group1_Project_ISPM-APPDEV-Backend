0b030a0f95d31fde4f65453a1c6148ffef3b1949
Jamesnl55555 <jamesnl55555@gmail.com>
2025-11-20 02:15:09 +0800

    modified api route

--- routes/api.php
+++ routes/api.php
@@ -9,16 +9,6 @@ use App\Http\Controllers\ExcelController;
 use App\Models\Product;
 use App\Models\Transaction;
 
-Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-    Route::post('register', [RegisteredUserController::class, 'store']);
-    
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
-});
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
