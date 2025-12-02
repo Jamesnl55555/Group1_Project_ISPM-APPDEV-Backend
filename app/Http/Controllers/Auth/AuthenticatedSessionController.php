56a2bbfb60812ccaeb55a7da2a7774ef18bd2622
Jamesnl55555 <jamesnl55555@gmail.com>
2025-12-03 03:46:57 +0800

    Fixed Back

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -42,13 +42,10 @@ class AuthenticatedSessionController extends Controller
     */
     public function destroy(Request $request)
     {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-    
-
+        //storage the token
+        $token = $request->user()->currentAccessToken();
+        //revoke the token
+        $token->delete();
         return response()->json([
                 'canResetPassword' => Route::has('password.request'),
                 'success' => true,
