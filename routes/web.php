ac9dd11af01b0cefc4462157dcf8644f0f45597d
Jamesnl55555 <jamesnl55555@gmail.com>
2025-12-03 22:36:33 +0800

    fixing

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\NewPasswordController;
 
+Route::middleware('web')->get('/sanctum/csrf-cookie', function () {
+    return response()->json(['csrf' => true]);
+});
+
 
 
 Route::middleware(['web'])->group(function () {
