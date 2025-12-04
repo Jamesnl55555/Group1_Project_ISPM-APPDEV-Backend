5029545ade5949408e0c91b0981110c4b3fc3793
Jamesnl55555 <jamesnl55555@gmail.com>
2025-12-04 23:27:53 +0800

    Now token

--- config/database.php
+++ config/database.php
@@ -95,7 +95,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => 'require',
+            'options' => [ 
+                1012 => 2, 
+            ],
         ],
 
         'sqlsrv' => [
