22f133cfa7a61c3b82967f744c926f649dc58cce
Jamesnl55555 <jamesnl55555@gmail.com>
2025-12-05 20:07:43 +0800

    Modified Routes

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,9 @@ use GuzzleHttp\Middleware;
 Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
-Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
-Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
 
 
 Route::middleware('auth:sanctum')->group(function () {
