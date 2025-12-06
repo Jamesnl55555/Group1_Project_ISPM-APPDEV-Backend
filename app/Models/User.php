62da72a74794086c508e2726296fe9946d48aa7b
Jamesnl55555 <jamesnl55555@gmail.com>
2025-12-06 15:01:28 +0800

    fixing mail

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,42 +1,39 @@
 <?php
 
 namespace App\Models;
-use Laravel\Sanctum\HasApiTokens;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
 use App\Notifications\CustomVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
+     * Relationships
      */
-
     public function products(): HasMany
     {
         return $this->hasMany(Product::class);
     }
 
+    /**
+     * Mass assignable attributes
+     */
     protected $fillable = [
         'name',
         'email',
         'password',
-        'capital'
+        'capital',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
+     * Hidden attributes
      */
     protected $hidden = [
         'password',
@@ -44,18 +41,19 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
+     * Casts
      */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            
         ];
     }
+
+    /**
+     * Override Laravel's default email verification notification
+     */
     public function sendEmailVerificationNotification()
     {
         $this->notify(new CustomVerifyEmail());
