dc5baa0ed72f0a0bf6439f3e6627be8629216170
Jamesnl55555 <Jamesnl55555@gmail.com>
2026-02-28 13:35:37 +0800

    debugging for react native, added logs on PasswordResetLinkController

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Log;
 class PasswordResetLinkController extends Controller
 {
     public function store(Request $request)
-    {
+{
     $request->validate([
         'email' => 'required|email',
     ]);
@@ -21,22 +21,16 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
-        if ($status === Password::RESET_LINK_SENT) {
-            return response()->json([
-                'success' => true,
-                'message' => __($status),
-            ]);
-        }
-
-        throw ValidationException::withMessages([
-            'email' => [trans($status)],
-        ]);
-    } catch (\Exception $e) {
-        Log::error('Forgot password failed: '.$e->getMessage());
         return response()->json([
-            'success' => false,
-            'message' => 'Mail sending failed: '.$e->getMessage(),
+            'status' => $status
+        ]);
+
+    } catch (\Throwable $e) {
+        return response()->json([
+            'error' => true,
+            'message' => $e->getMessage(),
+            'trace' => $e->getTraceAsString(),
         ], 500);
     }
-    }
+}
 }
