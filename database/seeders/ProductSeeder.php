2e042a7c01d5ae9ec9028c8f0fe1f388d726e56d
Jamesnl55555 <Jamesnl55555@gmail.com>
2026-03-20 17:31:29 +0800

    just adding seeders to for placholder or fake datas

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -10,12 +10,12 @@ class ProductSeeder extends Seeder
 {
     public function run()
     {
-        $users = User::all();
-        foreach ($users as $user) {
-            Product::factory()->count(10)->create([
-                'user_id' => $user->id,
-                'file_path' => 'placeholder.jpg',
-            ]);
-        }
+        foreach (User::all() as $user) {
+        if ($user->products()->count() === 0) {
+        Product::factory()->count(10)->create([
+            'user_id' => $user->id,
+            'file_path' => 'placeholder.jpg',
+        ]);
+        }}
     }
 }
